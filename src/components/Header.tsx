133c4a49a04c3aab23fd8cb45addef7847347aba
rkartha <rohitkartha9@gmail.com>
2024-03-16 02:28:37 -0400

    basic header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,23 @@
 
+import Image from 'next/image'
+import Link from 'next/link'
+
 export function Header() {
     return (
       <>
-        <h1>HEADER</h1>
+        <nav class="bg-white border-gray-200 dark:bg-gray-900">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <Link href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <Image
+                src="/apple-touch-icon.png"
+                width={50}
+                height={50}
+                alt="Mic Logo"
+            />  
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Mic Finder</span>
+            </Link>
+        </div>
+        </nav>
       </>
     );
 }
